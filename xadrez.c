2d5b8b4eb3706c62edb37043ca806f6c07e86199
Mathyosa <teufofo.teu126@gmail.com>
2025-04-25 20:59:00 +0000

    Fase 2

--- xadrez.c
+++ xadrez.c
@@ -1,34 +1,63 @@
 #include <stdio.h>
 
-int main(){
-
-    int bispo, torre, rainha;
-    
-    bispo = 0; //Posição inicial do bispo
-    torre = 0; //Posição inicial da torre
-    rainha = 0; //Posição inicial da rainha
     
     //Movimento da torre
     
-    for (torre = 0; torre < 5; torre++) 
 
-    { 
-printf("Direita %d\n", torre);
-
-}
+    void movimento_torre(int casas) {
+        if (casas > 0) {
+            printf("Direita %d\n", casas);
+            movimento_torre(casas - 1);
+        }
+        
+    printf("\n");
+    }
+    
     //Movimento do bispo
    
-    while (bispo < 5) {
-    printf("Cima e Direita %d\n", bispo);
-        bispo++;
-    }
+   void movimento_bispo(int casas) 
+   {
+        if (casas > 0) {
+            printf("Cima e Direita \n", casas);
+            movimento_bispo(casas - 1);
+           
+            }
+            printf("\n");
+        }
+       
 
     //Movimento da rainha
 
-    do {
-    printf("Esquerda %d\n", rainha);
-        rainha++;
-    } while (rainha < 8);
+    void movimento_rainha(int casas) {
+        if (casas > 0) {
+            printf("Esquerda \n", casas);
+            movimento_rainha(casas - 1);
+        }
+        printf("\n");
+    }
+ 
+    int main() {
+    movimento_bispo(5); //Movimento do bispo
+    movimento_torre(5); //Movimento da torre
+    movimento_rainha(8); //Movimento da rainha
+    printf ("\n");
+
+    
+    //Movimento do cavalo
+
+    for(int cavalo = 0; cavalo <3; cavalo++) {
+        printf("Cima %d\n", cavalo);
+
+        while (cavalo < 1) {
+            printf("Cima %d\n", cavalo);
+            cavalo++;
+        }
+        do {
+            printf("Direita %d\n", cavalo);
+            cavalo++;
+        } while (cavalo < 2);
+    }
+    printf ("\n");
 
     return 0;
 }
